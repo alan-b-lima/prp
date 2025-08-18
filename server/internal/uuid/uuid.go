8d01db4a12bf8e7468f6d304404f4fb10f53ece6
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-08-18 12:02:44 -0300

    fix endianess of UUID's timestamp

--- server/internal/uuid/uuid.go
+++ server/internal/uuid/uuid.go
@@ -1,12 +1,13 @@
 // Copyright 2025 Alan Barbosa Lima
 // Licensed under the Apache License, Version 2.0
 
-// This package implements a UUID generator, accourding to UUID
-// Version 7 (based on the UNIX Epoch). This description can be found
-// in RFC9562 section 5.3.
+// This package implements a UUID generator and some other
+// functionalities revolving UUIDs. Read in RFC9562 for more info
+// about UUIDs.
 package uuid
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
 	"sync"
@@ -16,9 +17,7 @@ import (
 // This represents a uint128 UUID type. Elements of this type can,
 // and should, be compared using the == operator. The zero value of
 // UUID is classified as a Nil UUID in RFC9562 section 5.9.
-type UUID struct {
-	lo, hi uint64
-}
+type UUID [16]byte
 
 const (
 	_62BitMask = (1 << 62) - 1
@@ -32,14 +31,32 @@ var (
 )
 
 func init() {
-	lo, hi := uint64(time.Now().UnixNano()), uint64(0)
-	hi *= lo * 0xCAFE_D0CE
+	lo := uint64(time.Now().UnixNano())
+	hi := lo * 0xCAFE_D0CE
 
 	source = rand.NewPCG(lo, hi)
 }
 
 // Generates a new UUID accourding to version 7. It's safe to call
 // this function from multiple goroutines.
+//
+// The memory layout of a UUIDv7 is as follows:
+//
+//   - Unix Timestamp: 48-bit big-endian unsigned number of the Unix
+//     Epoch timestamp in milliseconds. Occupies bits 0 through 47,
+//     octets 1 through 5.
+//
+//   - Version: 4-bit version field, set to 0b0111 (7). Occupies bits
+//     48 through 51, octet 6.
+//
+//   - Random A: 12-bit pseudorandom data to provide uniqueness.
+//     Occupies bits 52 through 63, octects 6 through 7.
+//
+//   - Variant: 2-bit variant field, set to 0b10. Occupies bits 64
+//     through 65, octet 8.
+//
+//   - Random B: 62-bit pseudorandom data to provide uniqueness.
+//     Occupies bits 66 through 127, octets 8 through 15.
 func NewUUIDv7() UUID {
 	const (
 		version = 0b0111
@@ -54,22 +71,67 @@ func NewUUIDv7() UUID {
 	mulock.Unlock()
 
 	return UUID{
-		lo: unixTimestamp<<0 | version<<48 | randA<<52,
-		hi: variant | randB<<2,
+		0x0: byte(unixTimestamp >> 0x28), 0x1: byte(unixTimestamp >> 0x20),
+		0x2: byte(unixTimestamp >> 0x18), 0x3: byte(unixTimestamp >> 0x10),
+		0x4: byte(unixTimestamp >> 0x08), 0x5: byte(unixTimestamp >> 0x00),
+
+		0x6: version<<4 | byte(randA>>8),
+		0x7: byte(randA),
+
+		0x8: variant<<6 | byte(randB>>0x38),
+		0x9: byte(randB >> 0x30), 0xA: byte(randB >> 0x28),
+		0xB: byte(randB >> 0x20), 0xC: byte(randB >> 0x18),
+		0xD: byte(randB >> 0x10), 0xE: byte(randB >> 0x08),
+		0xF: byte(randB >> 0x00),
 	}
+}
+
+var errBadSliceLength = errors.New("given slice does not has 16 bytes")
+
+// Converts an UUID from a byte slice.
+func FromBytes(bytes []byte) (UUID, error) {
+	if len(bytes) != 16 {
+		return UUID{}, errBadSliceLength
+	}
+
+	return UUID(bytes), nil
+}
+
+var errBadUUIDString = errors.New("given string could not be parsed correctly")
+
+// Converts an UUID from a string format.
+func FromString(str string) (UUID, error) {
+	if len(str) != 36 {
+		return UUID{}, errBadUUIDString
+	}
+
+	var uuid UUID
+	n, err := fmt.Sscanf(str,
+		"%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+		&uuid[0], &uuid[1], &uuid[2], &uuid[3],
+		&uuid[4], &uuid[5],
+		&uuid[6], &uuid[7],
+		&uuid[8], &uuid[9],
+		&uuid[10], &uuid[11], &uuid[12], &uuid[13], &uuid[14], &uuid[15],
+	)
+	if err !=nil {
+		return UUID{}, err
+	}
+	if n != 16 {
+		return UUID{}, errBadUUIDString
+	}
+
+	return uuid, nil
 }
 
 // Implements the interface [fmt.Stringer] on the UUID type.
 func (uuid UUID) String() string {
-	fst4HexByte := (uuid.lo & 0x0000_0000_FFFF_FFFF) >> 0
-	snd2HexByte := (uuid.lo & 0x0000_FFFF_0000_0000) >> 32
-	trd2HexByte := (uuid.lo & 0xFFFF_0000_0000_0000) >> 48
-	for2HexByte := (uuid.hi & 0x0000_0000_0000_FFFF) >> 0
-	fif6HexByte := (uuid.hi & 0xFFFF_FFFF_FFFF_0000) >> 16
-
 	return fmt.Sprintf(
-		"%08x-%04x-%04x-%04x-%012x",
-		fst4HexByte, snd2HexByte, trd2HexByte,
-		for2HexByte, fif6HexByte,
+		"%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+		uuid[0], uuid[1], uuid[2], uuid[3],
+		uuid[4], uuid[5],
+		uuid[6], uuid[7],
+		uuid[8], uuid[9],
+		uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15],
 	)
 }
