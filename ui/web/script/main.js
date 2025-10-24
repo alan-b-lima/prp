7075944449c51361d261b7bd2fc0444a9b85c460
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-10-24 20:07:19 -0300

    feat(ui): add home page

--- ui/web/script/main.js
+++ ui/web/script/main.js
@@ -1,19 +1,69 @@
+const THEME_TEXT = {
+    "light": "🌞",
+    "dark": "🌑",
+}
+
 function main() {
-    const properties = {
-        "darker": "--color-darker",
-        "dark": "--color-dark",
-        "neutral": "--color-neutral",
-        "light": "--color-light",
-        "lighter": "--color-lighter",
-        "primary": "--color-primary",
-        "secondary": "--color-secondary",
-        "accent": "--color-accent",
+    const light_dark = document.getElementById("light-dark")
+    if (light_dark !== null) {
+        light_dark.addEventListener("click", listener_light_dark.bind(null, light_dark))
+        listener_light_dark(light_dark)
     }
 
-    const css = window.getComputedStyle(document.documentElement)
+    const tabs = document.querySelector(".tabs")
+    if (tabs !== null) {
+        window.addEventListener("resize", listener_is_overflowing.bind(null, tabs, "overflowing"));
+    }
+}
 
-    for (const property in properties) {
-        console.log(property, css.getPropertyValue(properties[property]))
+function listener_light_dark(element) {
+    const html_classes = document.documentElement.classList
+    let theme
+
+    LookForTheme:
+    {
+        if (html_classes.contains("light")) {
+            html_classes.remove("light")
+
+            theme = "dark"
+            break LookForTheme
+        }
+
+        if (html_classes.contains("dark")) {
+            html_classes.remove("dark")
+
+            theme = "light"
+            break LookForTheme
+        }
+
+        let light_dark = localStorage.getItem("light-dark")
+
+        if (light_dark !== "light" && light_dark !== "dark") {
+            if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+                light_dark = "dark"
+            } else {
+                light_dark = "light"
+            }
+        }
+
+        theme = light_dark
+    }
+
+    html_classes.add(theme)
+    element.textContent = THEME_TEXT[theme]
+    localStorage.setItem("light-dark", theme)
+}
+
+function listener_is_overflowing(element, clazz) {
+    const rect = element.getBoundingClientRect()
+
+    const top = Math.min(0, rect.top) / rect.height >= 0
+    const bottom = (rect.bottom - window.innerHeight) / rect.height >= 0
+
+    if (top && bottom) {
+        element.classList.add(clazz);
+    } else {
+        element.classList.remove(clazz);
     }
 }
 
