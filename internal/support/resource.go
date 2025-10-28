50448025f1fed25a1b79571818669b4f7aa64083
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-10-27 22:12:47 -0300

    feat(support): add custom support functionality

---
+++ internal/support/resource.go
@@ -0,0 +1,57 @@
+package router
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/alan-b-lima/prp/pkg/errors"
+)
+
+type RouteFunc func(w http.ResponseWriter, r *http.Request) error
+
+func (fn RouteFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := fn(w, r)
+	if err == nil {
+		return
+	}
+
+	if err, ok := errors.AsType[*errors.Error](err); ok {
+		writeJsonError(w, err, toStatusCode(err.Kind))
+		return
+	}
+
+	writeJsonError(w, err, http.StatusInternalServerError)
+}
+
+func writeJsonError(w http.ResponseWriter, err error, status int) {
+	body, e := json.Marshal(err)
+	if e != nil {
+		http.Error(w, e.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+	w.Write(body)
+}
+
+var StatusCodes = map[errors.Kind]int{
+	errors.InvalidInput:       http.StatusBadRequest,
+	errors.Unauthorized:       http.StatusUnauthorized,
+	errors.PreconditionFailed: http.StatusPreconditionFailed,
+	errors.NotFound:           http.StatusNotFound,
+	errors.Conflict:           http.StatusConflict,
+
+	errors.Internal:    http.StatusInternalServerError,
+	errors.Unavailable: http.StatusServiceUnavailable,
+	errors.Timeout:     http.StatusRequestTimeout,
+	errors.BadGateway:  http.StatusBadGateway,
+}
+
+func toStatusCode(kind errors.Kind) int {
+	if status, in := StatusCodes[kind]; in {
+		return status
+	}
+
+	return http.StatusInternalServerError
+}
---
+++ internal/support/transport.go
@@ -0,0 +1,86 @@
+package router
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"regexp"
+	"strconv"
+
+	"github.com/alan-b-lima/prp/pkg/errors"
+	uuidpkg "github.com/alan-b-lima/prp/pkg/uuid"
+)
+
+var (
+	reContentTypeApplicationJson = regexp.MustCompile(`^\s*(\*/\*|application/(json|\*))\s*(;.*)?\s*$`)
+	reAcceptApplicationJson      = regexp.MustCompile(`(^|.*,)\s*(\*/\*|application/(json|\*))\s*(;.*)?\s*($|,.*)`)
+)
+
+func LimitAndOffset(offsetStr, limitStr string, offset, limit *int) error {
+	var errs [2]error
+
+	if offsetStr != "" {
+		*offset, errs[0] = strconv.Atoi(offsetStr)
+	}
+	if limitStr != "" {
+		*limit, errs[1] = strconv.Atoi(limitStr)
+	}
+
+	if errs != [2]error{nil, nil} {
+		return ErrBadOffsetOrLimit.New(errors.Join(errs[:]...))
+	}
+
+	return nil
+}
+
+func UUIDFromString(uuidStr string) (uuidpkg.UUID, error) {
+	uuid, err := uuidpkg.FromString(uuidStr)
+	if err != nil {
+		return uuidpkg.UUID{}, ErrBadUUID.New(err)
+	}
+
+	return uuid, nil
+}
+
+func DecodeJSON(req any, r *http.Request) error {
+	contentType := r.Header.Get("Content-Type")
+	if contentType == "" {
+		return ErrNoContentType
+	}
+
+	if !reContentTypeApplicationJson.MatchString(contentType) {
+		return ErrNoContentType
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		if err, ok := err.(*json.SyntaxError); ok {
+			return ErrJsonSyntax.New("JSON syntax error at"+strconv.FormatInt(err.Offset, 10), nil)
+		}
+
+		return err
+	}
+
+	return nil
+}
+
+func EncodeJSON(res any, status int, w http.ResponseWriter, r *http.Request) error {
+	accept := r.Header.Get("Accept")
+	if !reAcceptApplicationJson.MatchString(accept) {
+		return ErrNotAcceptableJson
+	}
+
+	var b bytes.Buffer
+	if err := json.NewEncoder(&b).Encode(res); err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+
+	if _, err := io.Copy(w, &b); err != nil {
+		return err
+	}
+
+	return nil
+}
