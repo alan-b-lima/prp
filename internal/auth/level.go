c99b600d713763357c1e5b68a8e823e49163d6da
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-11-14 17:34:00 -0300

    feat(auth): add validation methods for Level type

--- internal/auth/level.go
+++ internal/auth/level.go
@@ -5,14 +5,26 @@ type Level int
 const (
 	Unlogged Level = iota
 
+	valid_start
+
 	Admin
 	User
+
+	valid_end
 )
 
 var levelStrings = map[Level]string{
 	Admin:    "admin",
 	User:     "user",
 	Unlogged: "unlogged",
+}
+
+func (l Level) IsValid() bool {
+	return valid_start < l && l < valid_end
+}
+
+func (l Level) IsValidOrUnlogged() bool {
+	return l == Unlogged || valid_start < l && l < valid_end
 }
 
 func (l Level) String() string {
